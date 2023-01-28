bb59f9a7e440075cd854554c19f38910d55c2f41
LWJ1982 <120092392+LWJ1982@users.noreply.github.com>
2023-01-29 00:02:02 +0800

    Create q5.js

---
+++ q5.js
@@ -0,0 +1,15 @@
+
+/*
+    Task 1:
+    - Import the file `external.js`.
+    - Destructure the imported function.
+*/
+
+// Task 1: Add code here
+Function printSomething(){console.log(“I am printing something”);}
+Module.exports.print=printSomething;
+
+console.log( “i am printing something.”)
+console.log(printSomething)
+
+print();
